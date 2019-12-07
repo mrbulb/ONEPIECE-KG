bd9c1de04bb32a1de1a3a9ced33b6696f36bb35d
Unknown <1372699300@qq.com>
2019-12-07 15:39:34 +0800

    [9] modify parse_processed_manual_talkop_vivre_card.py to parse manual processed 9-（201812阿拉巴斯坦+白胡子海贼团）.txt
    9-（201812阿拉巴斯坦+白胡子海贼团）.txt

--- talkop/parse_processed_manual_talkop_vivre_card.py
+++ talkop/parse_processed_manual_talkop_vivre_card.py
@@ -11,7 +11,8 @@ data_dir  = './data/processed_manual_talkop_vivre_card'
 # 6-（201907恐怖船+象岛）
 # 7-（201904空岛+PH岛）
 # 8-（201905水之都+德岛）
-file_name = '8-（201905水之都+德岛）'
+# 9-（201812阿拉巴斯坦+白胡子海贼团）
+file_name = '9-（201812阿拉巴斯坦+白胡子海贼团）'
 suffix    = '.txt'
 vivre_card_path = os.path.join(data_dir, file_name + suffix)
 
