785e8455c601272e756af1c81accafe25b53f27b
Unknown <1372699300@qq.com>
2019-12-25 23:42:28 +0800

    [deepke] convert baidu ke data to deepke acceptable format
    将baidu ke的数据转换为deep ke可以接受输入的格式

---
+++ deepke-master/utils/check_data.py
@@ -0,0 +1,70 @@
+# encoding=utf-8
+"""
+
+@author: bulb
+
+@contact: hsl7698590@gmail.com
+
+@file: check_data.py
+
+@time: 2019/12/25 19:34
+
+@desc: 检查一下训练数据是否合法
+
+"""
+
+import os
+import re
+
+root = '/home/zenghao/ZJU_study/Knowledge_Graph/deepke/data/origin/'
+# relation.csv  test.csv  train.csv  valid.csv
+train_file = os.path.join(root, 'train.csv')
+valid_file = os.path.join(root, 'valid.csv')
+test_file  = os.path.join(root, 'test.csv')
+
+
+with open(valid_file) as f:
+    content = f.readlines()
+
+
+# 处理正则里面几个特殊的字符
+def preprocess_regex(regex):
+	regex = regex.replace('+', '\+')
+	regex = regex.replace('*', '\*')
+	regex = regex.replace('?', '\?')
+
+	return regex
+
+l = []
+for item in content[1:]:
+    item = item.strip()
+    # sentence,relation,head,head_offset,tail,tail_offset
+    if len(item.split(',')) != 6:
+        print('\n[Warning] length of split != 6\nsentence: {}\nlength: {}'.format(item, len(item.split(','))))
+
+    split_item = item.split(',')
+
+    sentence    = '，'.join(split_item[:-5])
+    relation    = split_item[-5]
+    head        = split_item[-4]
+    head_offset = split_item[-3]
+    tail        = split_item[-2]
+    tail_offset = split_item[-1]
+
+
+    head_regex = preprocess_regex(head)
+    tail_regex = preprocess_regex(tail)    
+    head_pattern = re.compile(head_regex, re.S)
+    tail_pattern = re.compile(tail_regex, re.S)
+    head_result = re.findall(head_pattern, sentence)
+    tail_result = re.findall(tail_pattern, sentence)
+    if len(head_result) == 0:
+        print('\n[Error] the head [{}] dose not occur in this sentence\nsentence: {}'.format(head, sentence))
+        exit(-1)
+    elif len(tail_result) == 0:
+        print('\n[Error] the tail [{}] dose not occur in this sentence\nsentence: {}'.format(tail, sentence))
+        exit(-1)
+    if len(head_result) != 1:
+        print('\n[Warning] head [{}] occur more than once in this sentence\nsentence: {}\noccur times: {}'.format(head, sentence, len(head_result)))
+    if len(tail_result) != 1:
+        print('\n[Warning] tail [{}] occur more than once in this sentence\nsentence: {}\noccur times: {}'.format(tail, sentence, len(tail_result)))
---
+++ deepke-master/utils/convert_baiduke2deepke.py
@@ -0,0 +1,183 @@
+# encoding=utf-8
+"""
+
+@author: bulb
+
+@contact: hsl7698590@gmail.com
+
+@file: convert_baiduke2deepke.py
+
+@time: 2019/12/25 22:44
+
+@desc: 将baidu ke的数据转为deepke接收的格式
+       baidu ke(Knowledge Extraction) 数据集地址：https://ai.baidu.com/broad/download
+
+"""
+import os
+from tqdm import tqdm
+
+root = '/home/zenghao/ZJU_study/Knowledge_Graph/baidu_ke'
+train_file  = os.path.join(root, 'train_data.json')
+dev_file    = os.path.join(root, 'dev_data.json')
+schema_file = os.path.join(root, 'all_50_schemas')
+
+output_root = '/home/zenghao/ZJU_study/Knowledge_Graph/deepke/data/baidu_ke/origin'
+output_train_file    = os.path.join(output_root, 'train.csv')
+output_valid_file    = os.path.join(output_root, 'valid.csv')
+output_test_file     = os.path.join(output_root, 'test.csv')
+output_relation_file = os.path.join(output_root, 'relation.csv')
+if not os.path.exists(output_root):
+	os.makedirs(output_root)
+
+instance_header = 'sentence,relation,head,head_offset,tail,tail_offset'
+relation_header = 'head_type,tail_type,relation,index\nNone,None,None,0'
+
+# baidu ke example
+# 
+# {'postag': [{'word': '骊声', 'pos': 'n'},
+#   {'word': '-', 'pos': 'w'},
+#   {'word': '张艾嘉', 'pos': 'nr'}],
+#  'text': '骊声-张艾嘉',
+#  'spo_list': [{'predicate': '歌手',
+#    'object_type': '人物',
+#    'subject_type': '歌曲',
+#    'object': '张艾嘉',
+#    'subject': '骊声'}]}
+# -----------------------
+# deepke example
+# 
+# sentence,relation,head,head_offset,tail,tail_offset
+# 人物简介李谷娜，号晚香女士，祖籍，浙江宁波,祖籍,李谷娜,4,浙江宁波,17
+error_list = []
+def process_baidu_ke(file_path):
+
+	print('\nProcess File: {}'.format(file_path))
+	with open(file_path) as f:
+		content = f.readlines()
+
+	deepke_instance_list = []
+	for item in tqdm(content):
+		item = eval(item)
+
+		sentence = item['text'].lower().replace(',', '，')
+		spo_list = item['spo_list']
+		for spo in spo_list:
+			head = spo['subject'].lower().replace(',', '，')
+			tail = spo['object'].lower().replace(',', '，')
+			relation = spo['predicate']
+
+			head_offset = sentence.find(head)
+			tail_offset = sentence.find(tail)
+
+			if head_offset == -1:
+				print('\n[Error] the head [{}] dose not occur in this sentence\nsentence: {}'.format(head, sentence))
+				error_list.append(item)
+				continue
+			if tail_offset == -1:
+				print('\n[Error] the tail [{}] dose not occur in this sentence\nsentence: {}'.format(tail, sentence))
+				error_list.append(item)
+				continue
+
+			record_item = '{},{},{},{},{},{}'.format(sentence,relation,head,head_offset,tail,tail_offset)
+			deepke_instance_list.append(record_item)
+
+	print('Sentence Number: {}'.format(len(content)))
+	print('Instance Number: {}'.format(len(deepke_instance_list)))
+
+	return deepke_instance_list
+
+# 将baidu ke中的relation转换为deepke接收的形式
+# -----------------------
+# baidu ke example
+# 
+# {"object_type": "地点", "predicate": "祖籍", "subject_type": "人物"}
+# {"object_type": "人物", "predicate": "父亲", "subject_type": "人物"}
+# {"object_type": "地点", "predicate": "总部地点", "subject_type": "企业"}
+# {"object_type": "地点", "predicate": "出生地", "subject_type": "人物"}
+# -----------------------
+# deepke example
+# 
+# head_type,tail_type,relation,index
+# None,None,None,0
+# 影视作品,人物,导演,1
+# 人物,国家,国籍,2
+def process_baidu_ke_relation(file_path):
+
+	print('\nProcess File: {}'.format(file_path))
+	with open(file_path) as f:
+		content = f.readlines()
+
+	index = 1 # 0是给默认的 `None,None,None,0`(P.S.: 怀疑这个就是给负样本)
+	deepke_relation_list = []
+	for item in content:
+		item = eval(item)
+
+		head_type = item['subject_type']
+		tail_type = item['object_type']
+		relation  = item['predicate']
+
+		record_item = '{},{},{},{},'.format(head_type,tail_type,relation,index)
+		deepke_relation_list.append(record_item)
+
+		index += 1
+
+	print('Relation Number: {}'.format(len(deepke_relation_list)))
+
+	return deepke_relation_list
+
+
+# 将转换得到的deepke接收格式的数据，写入文件中
+def write_data(data, header, file_path, max_num=None):
+	print('\nWrite File: {}'.format(file_path))
+
+	if max_num == None:
+		max_num = len(data)
+
+	with open(file_path, 'w') as f:
+		# 先写入header, 再写入data
+		f.write(header + '\n')
+		for item in data[:max_num]:
+			f.write(item + '\n')
+
+	get_distinct_sentence_instance_number(data)
+
+
+# 统计获得数据中不同句子和instance的个数
+def get_distinct_sentence_instance_number(data):
+	sentence_set = set()
+
+	for item in tqdm(data):
+		item_split = item.split(',')
+		sentence_set.add(item_split[0])
+
+	print('Sentence Number: {}'.format(len(sentence_set)))
+	print('Instance Number: {}'.format(len(data)))
+
+
+trainval_data = process_baidu_ke(train_file)
+test_data     = process_baidu_ke(dev_file)
+relation_data = process_baidu_ke_relation(schema_file)
+
+
+# 将 train_data.json 中的数据划分为 训练集和验证集
+# 将 dev_data.json 中的数据直接作为测试集
+# 目前将验证集大小设置为和测试集一样
+valid_size = len(test_data)
+train_size = len(trainval_data) - valid_size
+test_size  = len(test_data)
+
+# train
+write_data(data=trainval_data[:-valid_size], header=instance_header,
+	file_path=output_train_file)
+
+# valid
+write_data(data=trainval_data[-valid_size:], header=instance_header,
+	file_path=output_valid_file)
+
+# test
+write_data(data=test_data, header=instance_header,
+	file_path=output_test_file)
+
+# relation
+write_data(data=relation_data, header=relation_header,
+	file_path=output_relation_file)
