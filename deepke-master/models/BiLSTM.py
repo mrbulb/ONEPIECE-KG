c9e0ce3e0f4c86d39b574d5b5f68f9769ae5596c
Unknown <1372699300@qq.com>
2019-12-25 22:43:32 +0800

    [deepke] init the repo from https://github.com/zjunlp/deepke

---
+++ deepke-master/models/BiLSTM.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+from . import BasicModule
+from module import Embedding, RNN
+
+
+class BiLSTM(BasicModule):
+    def __init__(self, cfg):
+        super(BiLSTM, self).__init__()
+
+        if cfg.dim_strategy == 'cat':
+            cfg.input_size = cfg.word_dim + 2 * cfg.pos_dim
+        else:
+            cfg.input_size = cfg.word_dim
+
+        self.embedding = Embedding(cfg)
+        self.bilstm = RNN(cfg)
+        self.fc = nn.Linear(cfg.hidden_size, cfg.num_relations)
+        self.dropout = nn.Dropout(cfg.dropout)
+
+    def forward(self, x):
+        word, lens, head_pos, tail_pos = x['word'], x['lens'], x['head_pos'], x['tail_pos']
+        inputs = self.embedding(word, head_pos, tail_pos)
+        out, out_pool = self.bilstm(inputs, lens)
+        output = self.fc(out_pool)
+
+        return output
